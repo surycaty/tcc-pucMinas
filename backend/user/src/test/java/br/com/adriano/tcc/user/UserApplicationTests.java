3a9e04f8b63d285d786c1ebcf4971f275e282393
Adriano Almeida <adriano.freire@ext.hdi.com.br>
2021-09-01 20:45:59 -0300

    commit test

--- backend/user/src/test/java/br/com/adriano/tcc/user/UserApplicationTests.java
+++ backend/user/src/test/java/br/com/adriano/tcc/user/UserApplicationTests.java
@@ -3,6 +3,7 @@ package br.com.adriano.tcc.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class UserApplicationTests {
 
