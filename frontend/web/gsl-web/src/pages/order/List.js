52e467446ee1943069a85e09c087a5e3958ce3f1
Adriano Almeida <surycaty.ibg@gmail.com>
2021-09-12 02:42:49 -0300

    Page Order list

--- frontend/web/gsl-web/src/pages/order/List.js
+++ frontend/web/gsl-web/src/pages/order/List.js
@@ -1,10 +1,26 @@
 import React from "react";
+import SmartDataTable from 'react-smart-data-table'
+import 'react-smart-data-table/dist/react-smart-data-table.css'
 
 export default class OrderList extends React.Component {
 
+      fetchDataImpl = [
+        { "id": "1", "name": "Computer", "detail": "link" },
+        { "id": "2", "name": "HD SSD", "detail": "link" },
+        { "id": "3", "name": "Kit Mouse Keyboard", "detail": "link" },
+      ]
+      
+
     render() {
         return(
-            <div>Minha Order List</div>
+            <div>
+                <h2>Minha Order List</h2>
+                <SmartDataTable
+                    data={this.fetchDataImpl}
+                    
+                />
+         </div>
+
         );
     }
 }
